023fc85c41441781ca4428fff55dc3f3a71b08af
yitian.yang <yangyit139@gmail.com>
2023-04-23 23:13:12 +0200

    add mile file

---
+++ train.py
@@ -0,0 +1,83 @@
+import git
+import os
+import socket
+import time
+
+import lightning.pytorch as pl
+# from pytorch_lightning.plugins import DDPPlugin
+# from lightning.pytorch.strategies import DDPStrategy
+from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
+from lightning.pytorch.callbacks import ModelSummary
+
+from mile.config import get_parser, get_cfg
+from mile.data.dataset import DataModule
+from mile.trainer import WorldModelTrainer
+
+from clearml import Task
+
+
+class SaveGitDiffHashCallback(pl.Callback):
+    def on_train_start(self, trainer, pl_model):
+        repo = git.Repo()
+        trainer.git_hash = repo.head.object.hexsha
+        trainer.git_diff = repo.git.diff(repo.head.commit.tree)
+
+    def on_save_checkpoint(self, trainer, pl_module, checkpoint):
+        checkpoint['world_size'] = trainer.world_size
+        checkpoint['git_hash'] = trainer.git_hash
+        checkpoint['git_diff'] = trainer.git_diff
+
+
+def main():
+    args = get_parser().parse_args()
+    cfg = get_cfg(args)
+
+    # task = Task.init(project_name='MasterThesis/world_model', task_name='MILE', task_type='training', tags=cfg.TAG)
+    # task.connect(cfg)
+
+    data = DataModule(cfg)
+    model = WorldModelTrainer(cfg.convert_to_dict())
+
+    save_dir = os.path.join(
+        cfg.LOG_DIR, time.strftime('%d%B%Yat%H:%M:%S%Z') + '_' + socket.gethostname() + '_' + cfg.TAG
+    )
+    logger = pl.loggers.TensorBoardLogger(save_dir=save_dir)
+
+    callbacks = [
+        SaveGitDiffHashCallback(),
+        ModelSummary(),
+        pl.callbacks.LearningRateMonitor(),
+        ModelCheckpoint(
+            save_dir, every_n_train_steps=cfg.VAL_CHECK_INTERVAL,
+        ),
+    ]
+
+    if cfg.LIMIT_VAL_BATCHES in [0, 1]:
+        limit_val_batches = float(cfg.LIMIT_VAL_BATCHES)
+    else:
+        limit_val_batches = cfg.LIMIT_VAL_BATCHES
+
+    replace_sampler_ddp = not cfg.SAMPLER.ENABLED
+
+    trainer = pl.Trainer(
+        # devices=cfg.GPUS,
+        accelerator='auto',
+        # strategy='ddp',
+        precision=cfg.PRECISION,
+        # sync_batchnorm=True,
+        max_epochs=None,
+        max_steps=cfg.STEPS,
+        callbacks=callbacks,
+        logger=logger,
+        log_every_n_steps=cfg.LOGGING_INTERVAL,
+        val_check_interval=cfg.VAL_CHECK_INTERVAL,
+        limit_val_batches=limit_val_batches,
+        # use_distributed_sampler=replace_sampler_ddp,
+        profiler='simple',
+    )
+
+    trainer.fit(model, datamodule=data)
+
+
+if __name__ == '__main__':
+    main()
